f2960306cc614b69b3fe81386fd4e818dca9e8ee
nguyenanhtu2907 <nguyenanhtuxx@gmail.com>
2020-11-18 22:09:21 +0700

    add front profile

--- src/resources/app/models/Post.js
+++ src/resources/app/models/Post.js
@@ -3,11 +3,10 @@ const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-generator');
 mongoose.plugin(slug);
 const Post = new Schema({
-    owner: { type: String, default: '' },
+    owner: { type: Object},
     title: { type: String },
     address: { type: String, default: '' },
     nearby: { type: String, default: '' },
-    phone: { type: String, default: '' },
     description: { type: String, default: '' },
     rentcost: { type: String, default: '' },
     roomtype: { type: String, default: '' },
@@ -15,6 +14,7 @@ const Post = new Schema({
     withowner: { type: Boolean, default: false },
     equipments: { type: Array, default: [] },
     images: { type: Array, default: [] },
+    comments: { type: Array, default: [] },
     availabletime: { type: Number, default: 7 },
     postcost: { type: Number, default: 0 },
     saved: { type: Number, default: 0 },
