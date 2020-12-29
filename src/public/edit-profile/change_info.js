9977676baee00f76f9b03f13d6d829d5a9aa5bed
Nguyễn Anh Tú <52185755+nguyenanhtu2907@users.noreply.github.com>
2020-12-29 18:14:07 +0700

    Revert "Revert "Anhtu""

---
+++ src/public/edit-profile/change_info.js
@@ -0,0 +1,162 @@
+//preview image
+var loader = function (evt){
+  let file = evt.target.files;
+  let output = document.getElementById("selector");
+  if(file[0].type.match("image")) { 
+      let reader = new FileReader();
+      reader.addEventListener("load", function(e) {
+          let data = e.target.result;
+          let image = document.createElement("img");
+          image.src = data;
+          document.querySelector('#avatarText').value=data;
+          output.innerHTML = "";
+          output.insertBefore(image,null);
+          output.classList.add("image")
+      });
+      reader.readAsDataURL(file[0]);
+  }
+}
+let fileInput = document.getElementById("avatar");
+fileInput.addEventListener("change", loader);
+
+
+$(document).ready(function () {
+  var info = document.getElementsByClassName("change_info");
+  var pass = document.getElementsByClassName("change_pass");
+  $(pass).click(function () {
+    history.pushState({ edit: 1 }, '', '?type=password')
+  });
+  $(info).click(function () {
+    history.pushState({ edit: 1 }, '', '?type=info')
+  });
+})
+
+function getSelectedOption(sel) {
+var opt;
+for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+    opt = sel.options[i];
+    if ( opt.selected === true ) {
+        break;
+    }
+}
+return opt;
+}
+
+//fetch province 
+var ProvinceSel = document.getElementById('province')
+var x = getSelectedOption(ProvinceSel)
+// console.log(x)
+var DistrictSel = document.getElementById('district')
+
+//fetch province 
+var provinceApi =
+"https://online-gateway.ghn.vn/shiip/public-api/master-data/province";
+
+var myHeader = new Headers({
+
+'Content-Type': 'application/json',
+'Token': '7a07ad56-3965-11eb-b36a-0e2790f48b9c',
+}
+
+);
+
+var object = {
+method: 'GET',
+headers: myHeader
+}
+
+
+fetch(provinceApi, object)
+.then( res => res.json() )
+.then(posts => {
+  
+ 
+   var htmls = posts.data.map(function(post){
+       return ` <option value="${post.ProvinceName}" title="${post.ProvinceID}">
+       ${post.ProvinceName}
+       </option>`
+        
+
+    })
+    // console.log(posts);
+   htmls.join('');
+   document.getElementById('province').innerHTML += htmls;
+})
+
+//fetch district   
+var provinceInput = document.getElementById('province')
+
+var districtApi =
+"https://online-gateway.ghn.vn/shiip/public-api/master-data/district";
+
+
+fetch(districtApi, object)
+.then(res=>res.json())
+.then(posts => {
+
+provinceInput.onchange = function(){
+  var x = getSelectedOption(ProvinceSel)
+  
+  // console.log(ProvinceValue);
+  var provinceValue =  x.title;
+  
+  // console.log(typeof (provinceValue));
+  var htmls = posts.data.map(function(post){
+    // console.log(typeof(post.DistrictID))
+    // console.log(post.ProvinceID == Number(provinceValue))
+      if(post.ProvinceID == Number(provinceValue)){
+        return ` <option value="${post.DistrictName}" title="${post.DistrictID}">
+             ${post.DistrictName}
+            </option>`
+      }
+    })
+    
+    
+//  console.log(posts)
+htmls.join('');
+var a=`<option selected  value="" disabled >Quận huyện </option>`;
+document.getElementById('district').innerHTML = a;
+document.getElementById('district').innerHTML += htmls;
+}
+   
+})
+
+
+//fetch ward
+var districtInput = document.getElementById('district')
+
+var wardApi = "https://online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id";
+fetch(wardApi, object)
+.then(res => res.json())
+.then(posts => {
+districtInput.onchange = function(){
+  var x = getSelectedOption(DistrictSel)
+  var districtValue = x.title;
+  // console.log(typeof (provinceValue));
+  var htmls = posts.data.map(function(post){
+    // console.log(typeof(post.DistrictID))
+    // console.log(post.ProvinceID == Number(provinceValue))
+      if(post.DistrictID == Number(districtValue)){
+        return ` <option value="${post.WardName}">
+             ${post.WardName}
+            </option>`
+      }
+    })
+    
+    
+//  console.log(posts)
+htmls.join('');
+document.getElementById('ward').innerHTML = htmls;
+}
+           
+                
+        
+    
+    
+
+})
+
+  
+
+
+
--- src/resources/app/models/Post.js
+++ src/resources/app/models/Post.js
@@ -5,15 +5,15 @@ const slug = require('mongoose-slug-generator');
 mongoose.plugin(slug);
 
 const Post = new Schema({
-    owner: { type: String},
+    owner: { type: String },
     title: { type: String },
     address: { type: Object, default: '' },
     contact: { type: String, default: '' },
     nearby: { type: String, default: '' },
     description: { type: String, default: '' },
     rentcost: { type: String, default: '' },
-    electric: {type: String, default: ''},
-    water: {type: String, default: ''},
+    electric: { type: String, default: '' },
+    water: { type: String, default: '' },
     roomtype: { type: String, default: '' },
     area: { type: String, default: '' },
     infoOwner: { type: String, default: '' },
@@ -26,12 +26,14 @@ const Post = new Schema({
     viewed: { type: Number, default: 0 },
     statusrent: { type: Boolean, default: false },
     // fulfill: { type: Boolean, default: false },
-    checked: {type: Number, default: 0},
+    checked: { type: Number, default: 0 },
+    extendedTime: { type: Number, default: 0 },
     rates: { type: Array, default: [] },
     key: { type: String, default: '' },
-    slug: {type: String, slug: 'title', unique: true},
+    isReported: { type: Boolean, default: false },
+    slug: { type: String, slug: 'title', unique: true },
 }, {
     timestamps: true,
 })
-Post.index({key: 'text'});
+Post.index({ key: 'text' });
 module.exports = mongoose.model('Post', Post);
