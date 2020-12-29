ece1b10281388e913ce2fd1b1c40c2fd91255ef6
Vinh <dothanhvinh0912@gmail.com>
2020-12-29 18:15:55 +0700

    update Home Screen

--- front-end/HomeScreen/home.js
+++ front-end/HomeScreen/home.js
@@ -1,6 +1,12 @@
-var cities = document.getElementsByName("city");
-var districts = document.getElementsByName("districts");
-var cost = document.getElementsByName("city");
+var cities; // cac thanh pho
+var iCity; // index city duoc click
+var iDistrict; //index district duoc click
+fetch("http://127.0.0.1:5500/front-end/searchResult/local.json")
+  .then((res) => res.json())
+  .then((data) => {
+    cities = data;
+  })
+  .catch();
 
 const obj = [
   {
@@ -12,25 +18,6 @@ const obj = [
   },
 ];
 
-// const getAPI = async () => {
-//   const response = await fetch(
-//     "https://dc.tintoc.net/app/api-customer/public/provinces"
-//   );
-//   const cities = response.json();
-//   try {
-//     let html = "";
-//     cities.then((city) => {
-//       city.forEach((data) => {
-//         html += `
-//         <option>${data.name}</option>
-//         `;
-//         document.querySelector("#city").innerHTML += html;
-//       });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 function getSelectedOption(sel) {
   var opt;
   for (var i = 0, len = sel.options.length; i < len; i++) {
@@ -42,41 +29,41 @@ function getSelectedOption(sel) {
   return opt;
 }
 
-var ProvinceLink ="https://online-gateway.ghn.vn/shiip/public-api/master-data/province";
+var ProvinceLink =
+  "https://online-gateway.ghn.vn/shiip/public-api/master-data/province";
 var myheader = new Headers({
-  'Content-Type': 'application/json',
-  'Token': '7a07ad56-3965-11eb-b36a-0e2790f48b9c',
-})
- var Myobject ={
-   method : 'GET',
-   headers : myheader
-  }
-   
+  "Content-Type": "application/json",
+  Token: "7a07ad56-3965-11eb-b36a-0e2790f48b9c",
+});
+var Myobject = {
+  method: "GET",
+  headers: myheader,
+};
+
 fetch(ProvinceLink, Myobject)
-.then (res => res.json())
-.then(posts => {
-  var htmls = posts.data.map(function(post){
-    return `<option value ="${post.ProvinceName}" title="${post.ProvinceID}">
+  .then((res) => res.json())
+  .then((posts) => {
+    var htmls = posts.data.map(function (post) {
+      return `<option value ="${post.ProvinceName}" title="${post.ProvinceID}">
     ${post.ProvinceName}
-    </option>`
-    // console.log(post.ProvinceId)
-  })
-  htmls.join('')
-  document.getElementById('city').innerHTML +=htmls;
-})
-var provinceInput = document.getElementById('province')
+    </option>`;
+      // console.log(post.ProvinceId)
+    });
+    htmls.join("");
+    document.getElementById("city").innerHTML += htmls;
+  });
+var provinceInput = document.getElementById("province");
 
 var districtApi =
   "https://online-gateway.ghn.vn/shiip/public-api/master-data/district";
 
-var cityInput = document.getElementById('city')
+var cityInput = document.getElementById("city");
 
 fetch(districtApi, Myobject)
-  .then(res => res.json())
-  .then(posts => {
-
+  .then((res) => res.json())
+  .then((posts) => {
     cityInput.onchange = function () {
-      var x = getSelectedOption(cityInput)
+      var x = getSelectedOption(cityInput);
 
       // console.log(ProvinceValue);
       var provinceValue = x.title;
@@ -88,72 +75,110 @@ fetch(districtApi, Myobject)
         if (post.ProvinceID == Number(provinceValue)) {
           return ` <option value="${post.DistrictName}" title="${post.DistrictID}">
                    ${post.DistrictName}
-                  </option>`
+                  </option>`;
         }
-      })
-
+      });
 
       //  console.log(posts)
-      htmls.join('');
+      htmls.join("");
       var a = `<option selected  value="" disabled >Quận huyện </option>`;
-      document.getElementById('district').innerHTML = a;
-      document.getElementById('district').innerHTML += htmls;
+      document.getElementById("district").innerHTML = a;
+      document.getElementById("district").innerHTML += htmls;
+    };
+  });
+
+function showAddress(e, step, status) {
+  let steps = Array.from(document.querySelectorAll(".select .option"));
+  let address = document.querySelector(".select>span");
+  if (step == 0) {
+    let html = "";
+    cities.forEach((city) => {
+      html += `
+          <li class="li-option" onclick="showAddress(event, 1, 1)">${city.name}<div class="fa fa-angle-right"></div></li>
+          `;
+    });
+    steps[step].querySelector(".list-option").innerHTML = html;
+  }
+  if (step == 1) {
+    if (!e.target.classList.contains("back")) {
+      iCity = Array.from(e.target.parentNode.querySelectorAll("li")).indexOf(
+        e.target
+      );
     }
-
-  })
-
-
-
-function showList(e, element){
-  document.querySelector(element).classList.toggle('none');
-}
-
-// function switchNum(a, b){
-//   a = tmp;
-//   a=b;
-//   b=tmp;
-// }
-
-function checkPrice(e){
-  let text = '';
-  if(e.target.name == 'min'){
-      min = e.target;
-  }else{
-      max = e.target;
-      if(min.value*1 <= max.value*1){
-          text = min.parentNode.innerText + ' - ' + max.parentNode.innerText;
-          document.querySelector('.minPrice').value=min.value;
-          document.querySelector('.maxPrice').value=max.value;
-      }else{
-          text = max.parentNode.innerText + ' - ' + min.parentNode.innerText;
-          document.querySelector('.maxPrice').value=min.value;
-          document.querySelector('.minPrice').value=max.value;
-      }
-      document.querySelector('.price>span').innerText = text;
-      document.querySelector('.price>span').innerHTML += '<div class="fa fa-angle-down"></div>'
-      document.querySelector('.price .option').classList.add('none');
+    let html =
+      '<li class="back" onclick="showAddress(event, 0, -1)"><div class="fa fa-angle-left"></div>Quay lại </li>';
+    cities[iCity].districts.forEach((district) => {
+      html += `
+          <li class="li-option" onclick="showAddress(event, 2, 1)">${district.name}<div class="fa fa-angle-right"></div></li>
+          `;
+    });
+    steps[step].querySelector(".list-option").innerHTML = html;
+  }
+  if (step == 2 && status != 0) {
+    if (!e.target.classList.contains("back")) {
+      iDistrict = Array.from(
+        e.target.parentNode.querySelectorAll("li")
+      ).indexOf(e.target);
+    }
+    let html =
+      '<li class="back" onclick="showAddress(event, 1, -1)"><div class="fa fa-angle-left"></div>Quay lại </li>';
+    cities[iCity].districts[iDistrict - 1].wards.forEach((ward) => {
+      html += `
+          <li class="li-option" onclick="showAddress(event, 2, 0)">${ward.name}<div class="fa fa-angle-right"></div></li>
+          `;
+    });
+    steps[step].querySelector(".list-option").innerHTML = html;
+  }
+  if (
+    e.target.classList.contains("back") ||
+    e.target.classList.contains("fa-angle-down")
+  ) {
+    address.innerText = "Chọn khu vực";
+    address.innerHTML += '<div class="fa fa-angle-down"></div>';
+    steps[step].classList.remove("none");
+    steps[step + 1].classList.add("none");
+  } else {
+    if (step == 0) {
+      steps[step].classList.remove("none");
+    } else {
+      steps[step].classList.remove("none");
+      steps[step - 1].classList.add("none");
+    }
+    address.innerText = e.target.innerText;
+    address.innerHTML += '<div class="fa fa-angle-down"></div>';
+  }
+  if (step == 2 && status == 0) {
+    steps.forEach((option, index) => {
+      option.classList.add("none");
+    });
+  }
+  if (step != 0) {
+    e.target.parentNode.parentNode.querySelector("input").value =
+      e.target.innerText;
   }
 }
+function showList(e, element) {
+  document.querySelector(element).classList.toggle("none");
+}
 
-// const getAPI_2 = async () => {
-//   const response_2 = await fetch(
-//     "https://dc.tintoc.net/app/api-customer/public/districts?provinceId.equals=50"
-//   );
-//   const districts = response_2.json();
-//   try {
-//     let html = "";
-//     districts.then((district) => {
-//       district.forEach((data) => {
-//         html += `
-//         <option>${data.name}</option>
-//         `;
-//         document.querySelector("#district").innerHTML += html;
-//       });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// getAPI();
-// getAPI_2();
+function checkPrice(e) {
+  let text = "";
+  if (e.target.name == "min") {
+    min = e.target;
+  } else {
+    max = e.target;
+    if (min.value * 1 <= max.value * 1) {
+      text = min.parentNode.innerText + " - " + max.parentNode.innerText;
+      document.querySelector(".minPrice").value = min.value;
+      document.querySelector(".maxPrice").value = max.value;
+    } else {
+      text = max.parentNode.innerText + " - " + min.parentNode.innerText;
+      document.querySelector(".maxPrice").value = min.value;
+      document.querySelector(".minPrice").value = max.value;
+    }
+    document.querySelector(".price>span").innerText = text;
+    document.querySelector(".price>span").innerHTML +=
+      '<div class="fa fa-angle-down"></div>';
+    document.querySelector(".price .option").classList.add("none");
+  }
+}
